f76ddf66a4a9af37cdb889aa9ea719b85ed6af43
caojunyan <2433027313@qq.com>
2018-04-11 21:18:11 +0800

    歌手页面

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,18 +1,92 @@
 <template>
 <div>
- 歌手页面
+ <div class="singer">
+    <listview :data="singers"></listview>
+ </div>
 </div>
 
 </template>
 
 <script>
+  import {getSingerList} from '../../api/singer'
+  import {ERR_OK} from '../../api/config'
+  import Singer from '../../common/js/singer'
+  import listview from '../../base/listview/listview.vue'
+  const HOT_NAME='热门';
+  const HOT_SINGER_LEN=10;
     export default {
+      data(){
+        return{
+          singers:[]
+        }
+      },
+      created(){
+        this._getSingerList()
+      },
+      methods:{
+        _getSingerList(){
+          getSingerList().then(res=>{
+            if(res.code===ERR_OK){
+              this.singers=res.data.list
+              this.singers=this._normalizeSinger(this.singers);
+            }
+          })
+        },
+        _normalizeSinger(list){
+          let map={
+            hot:{
+              title:HOT_NAME,
+              items:[]
+            }
+          }
+          list.forEach((item,index)=>{
+            if(index<HOT_SINGER_LEN){
+              map.hot.items.push(new Singer({
+                id:item.Fsinger_mid,
+                name:item.Fsinger_name
+              }))
+            }
+            const key=item.Findex
+            if(!map[key]){
+              map[key]={
+                title:key,
+                items:[]
+              }
+            }
+            map[key].items.push(new Singer({
+              id:item.Fsinger_mid,
+              name:item.Fsinger_name
+            }))
+          })
+          let hot=[]
+          let ret=[]
+          for(let key in map){
 
+            let val=map[key]
+            if(val.title.match(/[a-zA-Z]/)){
+              ret.push(val)
+            }else if(val.title==="热门"){
+              hot.push(val)
+            }
+          }
+          ret.sort((a,b)=>{
+            return a.title.charCodeAt(0)-b.title.charCodeAt(0)
+          })
+          return hot.concat(ret)
+        }
+      },
+      components:{
+        listview
+      }
 
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="stylus" rel="stylesheet/stylus">
-
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.singer
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
 </style>
